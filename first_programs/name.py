f3f57eb945abf4ab84ba3a59bede8488b5ccd1c0
CloveRs212 <cloverindustrys@gmail.com>
2023-11-17 22:14:54 -0500

    First code in Py

---
+++ first_programs/name.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+print('Helllooo ' + name + '!!')
+weather = input('How is the weather today? ')
+print('Wow it is wild that it is ' + weather + 'out right now.')
