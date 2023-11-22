3e5cc547e1f1c533e411168e91283059e240a3a6
CloveRs212 <cloverindustrys@gmail.com>
2023-11-21 23:29:09 -0500

    OOB lessions

---
+++ first_programs/new_objects.py
@@ -0,0 +1,23 @@
+class PlayerCharacter:
+    # Class Object Attribute
+    membership = True
+
+    def __init__(self, name='', age=0):
+        if age > 18:
+            self.name = name  # attributes
+            self.age = age
+
+    def shout(self):
+        print(f'My name is {self.name}')
+
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return num1 + num2
+
+
+player1 = PlayerCharacter('Carl', 44)
+player2 = PlayerCharacter('Tommy', 23)
+player2.attack = 44
+
+print(player1.run('hello'))
+print(player2.shout())
