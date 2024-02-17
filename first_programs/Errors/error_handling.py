54e55c78f03c149f7bca095c2611d44ac807d069
CloveRs212 <cloverindustrys@gmail.com>
2024-02-17 14:43:14 -0500

    Error Handling Section

--- first_programs/Errors/error_handling.py
+++ first_programs/Errors/error_handling.py
@@ -1,21 +1,27 @@
 # Error Handling
 ## instead of erroring out if someone inputs a word or last name, it will prompt the user to enter a number. Restarting the function over again till user completes it. Else exicutes right after it is done trying the function, then it will stop running at the break command.
-try:
-    age = int(input('what is your age?'))
-    print(age)
-except ValueError:
-    print('Please enter a number')
-except ZeroDivisionError:
-    print('Please enter age higher than 0')
-else: 
-    print('thank you!')
-    break
+while True:
+    try:
+        age = int(input('what is your age?'))
+        10/age
+        raise ValueError('hey cut it out')
+    except ValueError:
+        print('Please enter a number')
+        continue
+    except ZeroDivisionError:
+        print('Please enter age higher than 0')
+        break
+    else: 
+        print('thank you!')
+    finally:
+        print('ok, I am finally done')
+    print('can you hear me?')
 
 ###
-def sum(num1, num2):
-    try:
-        return num1 + num2
-    except TypeError as err:
-        print('Please Enter Numbers! ' + err)
+# def sum(num1, num2):
+#     try:
+#         return num1 + num2
+#     except TypeError as err:
+#         print('Please Enter Numbers! ' + err)
 
-print(sum('1', 2))
+# print(sum('1', 2))
